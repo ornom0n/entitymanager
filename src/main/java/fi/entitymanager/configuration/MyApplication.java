32b5cec95fe765502df775d36d31fcda51bf5d15
ornom0n <ornom0n@hotmail.com>
2018-10-17 14:23:56 +0300

    Modify folder structure and change default response

--- src/main/java/fi/entitymanager/configuration/MyApplication.java
+++ src/main/java/fi/entitymanager/configuration/MyApplication.java
@@ -2,7 +2,7 @@ package fi.entitymanager.configuration;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import fi.entitymanager.resources.EntityRest;
+import fi.entitymanager.rest.EntityRest;
 
 @ApplicationPath("/")
 public class MyApplication extends ResourceConfig {
